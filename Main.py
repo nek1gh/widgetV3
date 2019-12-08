0a26cc90c5d2fd536c16fb94ad0bbb3e9e29d3b4
nekigh <seven.nek1@gmail.com>
2019-12-08 16:26:40 +0500

    Version 1.0.2

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
-id = int(input())
-name = input()
-phone = input()
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+JSON = 'amocrm.json'
+jsondata = json.load(open(JSON))
+data = json.dumps(jsondata, sort_keys=True)
+dataJ = json.loads(data)
+
+id = dataJ['_embedded']['items'][4]['id']
+name = 11
+phone = 12
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open("n1first").sheet1
