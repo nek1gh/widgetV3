02c6b6289cc232ee1c4a32ea507a7ddd8f4ad95f
nekigh <seven.nek1@gmail.com>
2019-12-08 21:10:39 +0500

    Version 1.0.8

--- Main.py
+++ Main.py
@@ -29,33 +29,35 @@ row = 2
 for i in data:
     id = i['id']
     name = i['name']
+    custom_fields_id = '1'
+    custom_fields_name = '2'
+    custom_fields_id_values_value = '3'
+    custom_fields_id_values_enum = '4'
     for i2 in (i['custom_fields']):
         if len(i2) != 0:
             custom_fields_id = i2['id']
             custom_fields_name = i2['name']
-            custom_fields_id_values_value = ' '
-            custom_fields_id_values_enum = ' '
+            custom_fields_id_values_value = '5'
+            custom_fields_id_values_enum = '6'
             for i3 in (i2['values']):
                 custom_fields_id_values_value = i3['value']
                 if len(i2['values']) != 1:
                     custom_fields_id_values_enum = i3['enum']
                 elif len(i2['values']) == 1:
-                    custom_fields_id_values_enum = ' '
+                    custom_fields_id_values_enum = '7'
                 else:
-                    custom_fields_id_values_enum = ' '
+                    custom_fields_id_values_enum = '8'
         else:
-            custom_fields_id = ' '
-            custom_fields_name = ' '
-            custom_fields_id_values_value = ' '
-            custom_fields_id_values_enum = ' '
+            custom_fields_id = '1'
+            custom_fields_name = '2'
+            custom_fields_id_values_value = '3'
+            custom_fields_id_values_enum = '4'
         print(id)
         print(name)
         print(custom_fields_id)
         print(custom_fields_name)
         print(custom_fields_id_values_value)
         print(custom_fields_id_values_enum)
-
-
 
     sheet = client.open("n1first").sheet1
     data = [id, name, custom_fields_id, custom_fields_name, custom_fields_id_values_value, custom_fields_id_values_enum]
