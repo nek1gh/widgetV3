935e26b58f9d0dd98827d6f4973c924ffde02b99
nekigh <seven.nek1@gmail.com>
2019-12-08 19:47:44 +0500

    Version 1.0.4

--- Main.py
+++ Main.py
@@ -3,18 +3,46 @@ import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 JSON = 'amocrm.json'
-jsondata = json.load(open(JSON))
-data = json.dumps(jsondata, sort_keys=True)
-dataJ = json.loads(data)
+jLoad = json.load(open(JSON))
+jDumps = json.dumps(jLoad, sort_keys=True)
+jData = json.loads(jDumps)
+data = jData['_embedded']['items']
+number = len(data)
+print(type(data))
+print(number)
+for num in data:
+    for i in (num['custom_fields']):
+        print(i)
 
-id = dataJ['_embedded']['items'][4]['id']
-name = 11
-phone = 12
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-client = gspread.authorize(creds)
-sheet = client.open("n1first").sheet1
-data = [id, name, phone]
-row = 2
-sheet.insert_row(data, row)
+# i = 1
+# l = 0
+# for num in data:
+#     if len() != 0:
+#         print(i)
+#     else:
+#         print(l)
+
+# phone = 12
+# two = 1
+# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+#          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+# creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+# client = gspread.authorize(creds)
+# row = 2
+# for i in data:
+#     id = i['id']
+#     name = i['name']
+#     if len(i['custom_fields']) != 0:
+#         custom_fields_id = i['custom_fields']['id']
+#         custom_fields_name = i['custom_fields']['name']
+#         if len (i['custom_fields']['values']) != 1:
+#             custom_fields_id_values = i['custom_fields']['values']['value']
+#             custom_fields_id_values = i['custom_fields']['values']['value']
+#     else:
+#         custom_fields_id = ' '
+#         custom_fields_name = ' '
+#         custom_fields_id_values = ' '
+#     sheet = client.open("n1first").sheet1
+#     data = [id, name, phone, two]
+#     sheet.insert_row(data, row)
+#     row = +row
