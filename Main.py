94fc12d4b87c3cd5cb51dfffe2ce04e3f80f5d1f
nekigh <seven.nek1@gmail.com>
2019-12-08 20:18:16 +0500

    Version 1.0.6

--- Main.py
+++ Main.py
@@ -7,12 +7,12 @@ jLoad = json.load(open(JSON))
 jDumps = json.dumps(jLoad, sort_keys=True)
 jData = json.loads(jDumps)
 data = jData['_embedded']['items']
-number = len(data)
-print(type(data))
-print(number)
-for num in data:
-    for i in (num['custom_fields']):
-        print(i)
+# number = len(data)
+# print(type(data))
+# print(number)
+# for num in data:
+#     for i in (num['custom_fields']):
+#         print(i)
 # i = 1
 # l = 0
 # for num in data:
@@ -21,27 +21,44 @@ for num in data:
 #     else:
 #         print(l)
 
-# phone = 12
-# two = 1
-# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-#          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-# creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-# client = gspread.authorize(creds)
-# row = 2
-# for i in data:
-#     id = i['id']
-#     name = i['name']
-#     if len(i['custom_fields']) != 0:
-#         custom_fields_id = i['custom_fields']['id']
-#         custom_fields_name = i['custom_fields']['name']
-#         if len (i['custom_fields']['values']) != 1:
-#             custom_fields_id_values = i['custom_fields']['values']['value']
-#             custom_fields_id_values = i['custom_fields']['values']['value']
-#     else:
-#         custom_fields_id = ' '
-#         custom_fields_name = ' '
-#         custom_fields_id_values = ' '
-#     sheet = client.open("n1first").sheet1
-#     data = [id, name, phone, two]
-#     sheet.insert_row(data, row)
-#     row = +row
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+client = gspread.authorize(creds)
+row = 2
+for i in data:
+    id = i['id']
+    name = i['name']
+    for i2 in (i['custom_fields']):
+        if len(i2) != 0:
+            custom_fields_id = i2['id']
+            custom_fields_name = i2['name']
+            for i3 in (i2['values']):
+                if len(i2['values']) != 1:
+                    custom_fields_id_values_value = i3['value']
+                    custom_fields_id_values_enum = i3['enum']
+                elif len(i2['values']) == 1:
+                    custom_fields_id_values_value = i3['value']
+                    custom_fields_id_values_enum = ' '
+                else:
+                    custom_fields_id_values_value = ' '
+                    custom_fields_id_values_enum = ' '
+    else:
+        custom_fields_id = ' '
+        custom_fields_name = ' '
+        custom_fields_id_values = ' '
+
+        custom_fields_id_values_value = ' '
+        custom_fields_id_values_enum = ' '
+    print(id)
+    print(name)
+    print(custom_fields_id)
+    print(custom_fields_name)
+    print(custom_fields_id_values)
+    print(custom_fields_id_values_value)
+    print(custom_fields_id_values_enum)
+
+    sheet = client.open("n1first").sheet1
+    data = [id, name, custom_fields_id, custom_fields_name, custom_fields_id_values_value, custom_fields_id_values_enum]
+    sheet.insert_row(data, row)
+    row = +row
