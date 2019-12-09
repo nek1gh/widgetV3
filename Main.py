f1db7577d6bd65cdc0f98bbd4c6935bd124da2f3
developer <d@d.ru>
2019-12-09 15:20:35 +0300

    Version 1.0.10

--- Main.py
+++ Main.py
@@ -1,36 +1,51 @@
-import gspread
 import json
-from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+import datetime
+
+
+def change_date(timestamp):
+    return datetime.datetime.utcfromtimestamp(timestamp // 24 // 60 // 60 * 60 * 60 * 24)
+
+
+sJSON = 'status.json'
+dataStatus = json.load(open(sJSON))
+status = dataStatus['_embedded']['items']
+status_all = {status[funnel]['statuses'][i]['id']: status[funnel]['statuses'][i]['name'] for funnel in status for i
+              in status[funnel]['statuses']}
 
 JSON = 'amocrm.json'
-jLoad = json.load(open(JSON))
-jDumps = json.dumps(jLoad, sort_keys=True)
-jData = json.loads(jDumps)
-data = jData['_embedded']['items']
-# number = len(data)
-# print(type(data))
-# print(number)
-# for num in data:
-#     for i in (num['custom_fields']):
-#         print(i)
-# i = 1
-# l = 0
-# for num in data:
-#     if len() != 0:
-#         print(i)
-#     else:
-#         print(l)
+data = json.load(open(JSON))
+deal = data['_embedded']['items']
+# pprint(deal[12])
+for i in deal[:1]:
+    row_data = dict()
+    row_data['id'] = i['id']
+    row_data['status_id'] = status_all[i['status_id']]
+    row_data['company_id'] = i['company']['id'] if i['company'] != {} else ""
+    row_data['company_name'] = i['company']['name'] if i['company'] != {} else ""
+    row_data['created_at'] = str(change_date(i['created_at']))
+    row_data['updated_at'] = change_date(i['updated_at'])
+    pprint(row_data)
 
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-client = gspread.authorize(creds)
+'''
+ID	
+Имя статуса	
+ID компании	
+Имя компании	
+Дата создания	
+Дата завершения	
+Дата Отгрузки
+'''
+'''
 row = 2
 for i in data:
+    row_data = dict()
     row += 1
-    id = i['id']
-    name = i['name']
-    custom_fields_id = '1'
+    row_data['id'] = i['id']
+    row_data['name'] = i['name']
+    # print(type(row_data))
+    # pprint(row_data)
+ row_data["cust1"] = ""
     custom_fields_name = '2'
     custom_fields_id_values_value = '3'
     custom_fields_id_values_enum = '4'
@@ -53,14 +68,17 @@ for i in data:
             custom_fields_name = '10'
             custom_fields_id_values_value = '11'
             custom_fields_id_values_enum = '12'
+'''
+'''      
         print(id)
         print(name)
         print(custom_fields_id)
         print(custom_fields_name)
         print(custom_fields_id_values_value)
-        print(custom_fields_id_values_enum)
-
+        print(custom_fields_id_values_enum)'''
+'''
     sheet = client.open("n1first").sheet1
     data = [id, name, custom_fields_id, custom_fields_name, custom_fields_id_values_value, custom_fields_id_values_enum]
     sheet.insert_row(data, row)
 
+'''
